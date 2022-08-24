f95f42bfc0a4461b6756fda25ee62f2ba49efedf
Alexandre Benson Smith <benson@thorsoftware.com.br>
2022-08-23 21:29:15 -0300

    refactor: ensure SignUpController.handle implements correct interfaces

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from './signup'
+
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
